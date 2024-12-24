eeeac8ef32e558df3cded0c08c9af281f680ecba
ArtyFerro <117083185+ArtyFerro@users.noreply.github.com>
2024-12-24 18:40:56 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,26 @@
-from django.urls import path
-from django.views.generic import TemplateView
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
 
-app_name = 'pages'
+
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.internal_server_error'
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name='pages/about.html'),
-         name='about'),
-    path('rules/', TemplateView.as_view(template_name="pages/rules.html"),
-         name='rules'),
-]
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
